540c826fa607f6c2b5bc715f8e8195dd83337a06
Mihai Costea <mihai.andrei.costea@me.com>
2015-02-24 22:20:04 +0000

    Added CORS support

--- config/express.js
+++ config/express.js
@@ -6,7 +6,23 @@ module.exports = function() {
     var flash = require('connect-flash');
     var session = require('express-session');
     
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+          res.send(200);
+        }
+        else {
+          next();
+        }
+    };
+    
     var app = express();
+    
+    app.use(allowCrossDomain);
     
     app.use(bodyParser.urlencoded({
         extended: true
